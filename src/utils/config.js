7d1acc636224f44f41402c3e20547b3127082c70
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-24 19:03:04 -0400

    revealing API key for heroku

---
+++ src/utils/config.js
@@ -0,0 +1,4 @@
+const configAPI = "0e766ea1ed98d1ccf6d8def44ab51668";
+const mapAPI =
+  "pk.eyJ1Ijoia3hkYW5nIiwiYSI6ImNrMG12OXZ1cTFiZjAzY253ZmF6YmpidmkifQ.i9VSfIlEHuA79xq9fdmNVA";
+module.exports = { configAPI: configAPI, mapAPI: mapAPI };
