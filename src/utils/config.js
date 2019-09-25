4b1a3d8d3aa434d4448e0140001505f8bb68fcd8
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-25 11:20:51 -0400

    updating API keys for public repo

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,3 @@
-const configAPI = "0e766ea1ed98d1ccf6d8def44ab51668";
-const mapAPI =
-  "pk.eyJ1Ijoia3hkYW5nIiwiYSI6ImNrMG12OXZ1cTFiZjAzY253ZmF6YmpidmkifQ.i9VSfIlEHuA79xq9fdmNVA";
+const configAPI = "redacted - freshed new key hidden in Heroku environment";
+const mapAPI = "redacted - freshed new key hidden in Heroku environment";
 module.exports = { configAPI: configAPI, mapAPI: mapAPI };
